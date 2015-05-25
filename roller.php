8332e3ef6fcdde753b2513095bdba281dcb0cab9
scotchfield <swrittenb@gmail.com>
2015-05-24 23:06:08 -0400

    Add random dice rolling and variable storage/recall

--- roller.php
+++ roller.php
@@ -29,7 +29,14 @@ class WP_Roller {
 
 		self::$instance = $this;
 
+		mt_srand();
+
+		$this->state = array();
+
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+
+		add_shortcode( 'roller', array( $this, 'shortcode_roller' ) );
+		add_shortcode( 'roller_var', array( $this, 'shortcode_roller_var' ) );
 	}
 
 	/**
@@ -49,6 +56,31 @@ class WP_Roller {
 
 	}
 
+	public function shortcode_roller( $atts ) {
+		if ( isset( $atts[ 0 ] ) ) {
+			$pattern = '/(\d*)d(\d*)/';
+			if ( preg_match( $pattern, $atts[ 0 ], $match ) ) {
+				$result = 0;
+				$n = intval( $match[ 1 ] );
+				$d = intval( $match[ 2 ] );
+				for ( $i = 0; $i < $n; $i += 1 ) {
+					$result += mt_rand( 1, $d );
+				}
+
+				if ( isset( $atts[ 'var' ] ) ) {
+					$this->state[ $atts[ 'var' ] ] = $result;
+				}
+
+				return $result;
+			}
+		}
+	}
+
+	public function shortcode_roller_var( $atts ) {
+		if ( isset( $atts[ 0 ] ) && isset( $this->state[ $atts[ 0 ] ] ) ) {
+			return $this->state[ $atts[ 0 ] ];
+		}
+	}
 }
 
 $wp_roller = new WP_Roller();
