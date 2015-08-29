2055799dee2f8086a800a4dc3655c3f5af93422c
scotchfield <swrittenb@gmail.com>
2015-08-28 21:10:39 -0400

    Use test class reference to roller instance, not method reference

--- tests/test-roller.php
+++ tests/test-roller.php
@@ -47,9 +47,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::get_instance
 	 */
 	public function test_get_instance() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertNotNull( $class );
+		$this->assertNotNull( $this->class );
 	}
 
 	/**
@@ -72,8 +70,7 @@ class Test_Roller extends WP_UnitTestCase {
 	public function test_admin_menu() {
 		global $admin_page_hooks;
 
-		$class = WP_Roller::get_instance();
-		$class->admin_menu();
+		$this->class->admin_menu();
 
 		$this->assertArrayHasKey( 'roller_menu', $admin_page_hooks );
 	}
@@ -82,9 +79,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::get_list
 	 */
 	public function test_get_list_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertFalse( $class->get_list( 'does_not_exist' ) );
+		$this->assertFalse( $this->class->get_list( 'does_not_exist' ) );
 	}
 
 	/**
@@ -92,14 +87,12 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::get_list
 	 */
 	public function test_update_list_and_get_list() {
-		$class = WP_Roller::get_instance();
-
 		$list_id = 'test_list';
 		$list = "test\nlist";
 
-		$class->update_list( $list_id, $list );
+		$this->class->update_list( $list_id, $list );
 
-		$this->assertEquals( $list, $class->get_list( $list_id ) );
+		$this->assertEquals( $list, $this->class->get_list( $list_id ) );
 	}
 
 	/**
@@ -122,13 +115,11 @@ class Test_Roller extends WP_UnitTestCase {
 		$old_user_id = get_current_user_id();
 		wp_set_current_user( $user->ID );
 
-		$class = WP_Roller::get_instance();
-
 		ob_start();
-		$class->roller_page();
+		$this->class->roller_page();
 		ob_end_clean();
 
-		$this->assertInternalType( 'array', $class->lists );
+		$this->assertInternalType( 'array', $this->class->lists );
 
 		wp_set_current_user( $old_user_id );
 	}
@@ -142,17 +133,15 @@ class Test_Roller extends WP_UnitTestCase {
 		$old_user_id = get_current_user_id();
 		wp_set_current_user( $user->ID );
 
-		$class = WP_Roller::get_instance();
-
 		$list_id = 'test_list';
 		$_POST[ 'new_list' ] = true;
 		$_POST[ 'list_id' ] = $list_id;
 
 		ob_start();
-		$class->roller_page();
+		$this->class->roller_page();
 		ob_end_clean();
 
-		$this->assertNotFalse( $class->get_list( $list_id ) );
+		$this->assertNotFalse( $this->class->get_list( $list_id ) );
 
 		unset( $_POST[ 'list_id' ] );
 		unset( $_POST[ 'new_list' ] );
@@ -169,8 +158,6 @@ class Test_Roller extends WP_UnitTestCase {
 		$old_user_id = get_current_user_id();
 		wp_set_current_user( $user->ID );
 
-		$class = WP_Roller::get_instance();
-
 		$list_id = 'test_list';
 		$list_value = "test\nlist";
 		$_POST[ 'update_list' ] = true;
@@ -178,10 +165,10 @@ class Test_Roller extends WP_UnitTestCase {
 		$_POST[ 'list_value' ] = $list_value;
 
 		ob_start();
-		$class->roller_page();
+		$this->class->roller_page();
 		ob_end_clean();
 
-		$this->assertFalse( $class->get_list( $list_id ) );
+		$this->assertFalse( $this->class->get_list( $list_id ) );
 
 		unset( $_POST[ 'list_value' ] );
 		unset( $_POST[ 'list_id' ] );
@@ -199,11 +186,9 @@ class Test_Roller extends WP_UnitTestCase {
 		$old_user_id = get_current_user_id();
 		wp_set_current_user( $user->ID );
 
-		$class = WP_Roller::get_instance();
-
 		$list_id = 'test_list';
 
-		$class->update_list( $list_id, 'starting_value' );
+		$this->class->update_list( $list_id, 'starting_value' );
 
 		$list_value = "new\ntest\nlist";
 		$_POST[ 'update_list' ] = true;
@@ -211,10 +196,10 @@ class Test_Roller extends WP_UnitTestCase {
 		$_POST[ 'list_value' ] = $list_value;
 
 		ob_start();
-		$class->roller_page();
+		$this->class->roller_page();
 		ob_end_clean();
 
-		$this->assertEquals( $list_value, $class->get_list( $list_id ) );
+		$this->assertEquals( $list_value, $this->class->get_list( $list_id ) );
 
 		unset( $_POST[ 'list_value' ] );
 		unset( $_POST[ 'list_id' ] );
@@ -227,27 +212,21 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::expression
 	 */
 	public function test_expression_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEquals( 0, $class->expression( '' ) );
+		$this->assertEquals( 0, $this->class->expression( '' ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->shortcode_roller( array() ) );
+		$this->assertEmpty( $this->class->shortcode_roller( array() ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_invalid() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->shortcode_roller( array( 'bad_content' ) ) );
+		$this->assertEmpty( $this->class->shortcode_roller( array( 'bad_content' ) ) );
 	}
 
 	/**
@@ -255,9 +234,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_simple_die_roll() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEquals( '1', $class->shortcode_roller( array( '1d1' ) ) );
+		$this->assertEquals( '1', $this->class->shortcode_roller( array( '1d1' ) ) );
 	}
 
 	/**
@@ -265,9 +242,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_dice_roll_in_range() {
-		$class = WP_Roller::get_instance();
-
-		$result = intval( $class->shortcode_roller( array( '3d6' ) ) );
+		$result = intval( $this->class->shortcode_roller( array( '3d6' ) ) );
 
 		$this->assertTrue( $result >= 3 && $result <= 18 );
 	}
@@ -277,9 +252,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_simple_die_roll_plus() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEquals( '2', $class->shortcode_roller( array( '1d1+1' ) ) );
+		$this->assertEquals( '2', $this->class->shortcode_roller( array( '1d1+1' ) ) );
 	}
 
 	/**
@@ -287,18 +260,14 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller
 	 */
 	public function test_shortcode_roller_simple_die_roll_minus() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEquals( '0', $class->shortcode_roller( array( '1d1-1' ) ) );
+		$this->assertEquals( '0', $this->class->shortcode_roller( array( '1d1-1' ) ) );
 	}
 
 	/**
 	 * @covers WP_Roller::get_var
 	 */
 	public function test_get_var_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->get_var( 'does_not_exist' ) );
+		$this->assertEmpty( $this->class->get_var( 'does_not_exist' ) );
 	}
 
 	/**
@@ -307,14 +276,12 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::get_var
 	 */
 	public function test_shortcode_roller_dice_roll_stored() {
-		$class = WP_Roller::get_instance();
-
 		$var = 'test_var';
 
-		$result = $class->shortcode_roller( array( '1d1', 'var' => $var ) );
+		$result = $this->class->shortcode_roller( array( '1d1', 'var' => $var ) );
 
 		$this->assertEmpty( $result );
-		$this->assertEquals( '1', $class->get_var( $var ) );
+		$this->assertEquals( '1', $this->class->get_var( $var ) );
 	}
 
 	/**
@@ -322,9 +289,7 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller_var
 	 */
 	public function test_shortcode_roller_var_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->shortcode_roller_var( array() ) );
+		$this->assertEmpty( $this->class->shortcode_roller_var( array() ) );
 	}
 
 	/**
@@ -332,11 +297,9 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller_var
 	 */
 	public function test_shortcode_roller_var_does_not_exist() {
-		$class = WP_Roller::get_instance();
-
 		$var = 'test_var';
 
-		$this->assertEmpty( $class->shortcode_roller_var( array( 'var' => $var ) ) );
+		$this->assertEmpty( $this->class->shortcode_roller_var( array( 'var' => $var ) ) );
 	}
 
 	/**
@@ -345,31 +308,25 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller_var
 	 */
 	public function test_shortcode_roller_var_stored() {
-		$class = WP_Roller::get_instance();
-
 		$var = 'test_var';
 
-		$result = $class->shortcode_roller( array( '1d1', 'var' => $var ) );
+		$result = $this->class->shortcode_roller( array( '1d1', 'var' => $var ) );
 
-		$this->assertEquals( '1', $class->shortcode_roller_var( array( $var ) ) );
+		$this->assertEquals( '1', $this->class->shortcode_roller_var( array( $var ) ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_choose
 	 */
 	public function test_shortcode_roller_choose_empty() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->shortcode_roller_choose( array() ) );
+		$this->assertEmpty( $this->class->shortcode_roller_choose( array() ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_choose
 	 */
 	public function test_shortcode_roller_choose_list_does_not_exist() {
-		$class = WP_Roller::get_instance();
-
-		$this->assertEmpty( $class->shortcode_roller_choose( array( 'list' => 'does_not_exist' ) ) );
+		$this->assertEmpty( $this->class->shortcode_roller_choose( array( 'list' => 'does_not_exist' ) ) );
 	}
 
 	/**
@@ -377,14 +334,12 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller_choose
 	 */
 	public function test_shortcode_roller_choose_list_exists() {
-		$class = WP_Roller::get_instance();
-
 		$list_id = 'test_list';
 		$list = "test\nlist";
 
-		$class->update_list( $list_id, $list );
+		$this->class->update_list( $list_id, $list );
 
-		$result = $class->shortcode_roller_choose( array( 'list' => $list_id ) );
+		$result = $this->class->shortcode_roller_choose( array( 'list' => $list_id ) );
 
 		$this->assertTrue( in_array( $result, explode( "\n", $list ) ) );
 	}
@@ -395,86 +350,72 @@ class Test_Roller extends WP_UnitTestCase {
 	 * @covers WP_Roller::shortcode_roller_choose
 	 */
 	public function test_shortcode_roller_choose_list_exists_set_var() {
-		$class = WP_Roller::get_instance();
-
 		$var_id = 'test_var';
 		$list_id = 'test_list';
 		$list = "test\nlist";
 
-		$class->update_list( $list_id, $list );
+		$this->class->update_list( $list_id, $list );
 
-		$result = $class->shortcode_roller_choose( array( 'list' => $list_id, 'var' => $var_id ) );
+		$result = $this->class->shortcode_roller_choose( array( 'list' => $list_id, 'var' => $var_id ) );
 
 		$this->assertNull( $result );
-		$this->assertTrue( in_array( $class->get_var( $var_id ), explode( "\n", $list ) ) );
+		$this->assertTrue( in_array( $this->class->get_var( $var_id ), explode( "\n", $list ) ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_if
 	 */
 	public function test_shortcode_roller_if_empty() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
-		$this->assertEquals( $content, $class->shortcode_roller_if( array(), $content ) );
+		$this->assertEquals( $content, $this->class->shortcode_roller_if( array(), $content ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_if
 	 */
 	public function test_shortcode_roller_if_false() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
-		$this->assertEmpty( '', $class->shortcode_roller_if( array( 'bad_key' => 'bad_value' ), $content ) );
+		$this->assertEmpty( '', $this->class->shortcode_roller_if( array( 'bad_key' => 'bad_value' ), $content ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_loop
 	 */
 	public function test_shortcode_roller_loop_empty() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
-		$this->assertEmpty( $class->shortcode_roller_loop( array(), $content ) );
+		$this->assertEmpty( $this->class->shortcode_roller_loop( array(), $content ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_loop
 	 */
 	public function test_shortcode_roller_loop_invalid() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
-		$this->assertEmpty( $class->shortcode_roller_loop( array( 'bad_value' ), $content ) );
+		$this->assertEmpty( $this->class->shortcode_roller_loop( array( 'bad_value' ), $content ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_loop
 	 */
 	public function test_shortcode_roller_loop_single() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
-		$this->assertEquals( $content, $class->shortcode_roller_loop( array( '1' ), $content ) );
+		$this->assertEquals( $content, $this->class->shortcode_roller_loop( array( '1' ), $content ) );
 	}
 
 	/**
 	 * @covers WP_Roller::shortcode_roller_loop
 	 */
 	public function test_shortcode_roller_loop_multiple() {
-		$class = WP_Roller::get_instance();
-
 		$content = 'success';
 
 		$this->assertEquals(
 			$content . $content . $content,
-			$class->shortcode_roller_loop( array( '3' ), $content )
+			$this->class->shortcode_roller_loop( array( '3' ), $content )
 		);
 	}
 
